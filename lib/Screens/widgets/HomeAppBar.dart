f356c9e2412bbda5244040bb7846b3e56ba7b9b1
BaberHashmi512 <BaberHashmi512@gmail.com>
2023-04-13 03:01:21 +0500

    homeAppbar

--- lib/Screens/widgets/HomeAppBar.dart
+++ lib/Screens/widgets/HomeAppBar.dart
@@ -41,13 +41,13 @@ class HomeAppBar extends StatelessWidget {
             ),
             child: InkWell(
               onTap: () {
-                Navigator.pushNamed(context,"cartPage");
+                Navigator.pushNamed(context, "cartPage");
               },
-              child: Icon(
-                Icons.shopping_bag_outlined,
-                size: 32,
-                color: Color(0xFF4C53A5),
-              ),
+              // child: Icon(
+              //   Icons.shopping_bag_outlined,
+              //   size: 32,
+              //   color: Color(0xFF4C53A5),
+              // ),
             ),
           ),
         ],
